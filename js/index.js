0fc4b97ff52b8729043710663029c5edab573906
Asaf Sandro Smirnov <asafsmirnov@gmail.com>
2024-03-10 23:47:17 +0200

    Add the ability to play against a computer

--- js/index.js
+++ js/index.js
@@ -1,33 +1,91 @@
 const board = document.querySelectorAll('.col');
+const robotButton = document.querySelector('.icon-button');
+
 let moveCount = 0;
 let isXTurn = true;
+let isAgainstComputer = false;
+
+const handleOnRobotClick = () => {
+  if (!isAgainstComputer) {
+    robotButton.classList.add('icon-button-active');
+  } else {
+    robotButton.classList.remove('icon-button-active');
+  }
+  isAgainstComputer = !isAgainstComputer;
+  resetGame();
+};
 
 const handleOnSquareClick = (id) => {
-  const [row, col] = id.split('-')[1];
   const square = document.querySelector(`#${id}`);
+
   if (square.textContent) {
-    swal('Square is already taken. Please pick an empty one!');
-  } else {
-    square.textContent = isXTurn ? 'X' : 'O';
-    const didWin = checkForWin();
-    if (didWin) {
-      swal({
-        title: isXTurn ? 'X' : 'O',
-        text: "won this round!",
-        button: "Play again!",
-      });
-      return resetGame();
+    return swal('Square is already taken. Please pick an empty one!');
+  }
+
+  square.textContent = isXTurn ? 'X' : 'O';
+  afterMoveProcess();
+
+  if (isAgainstComputer && moveCount > 0) {
+    setTimeout(() => {
+      computerMove();
+      afterMoveProcess();
+    }, 300);
+  }
+};
+
+const afterMoveProcess = () => {
+  const didWin = checkForWin();
+  if (didWin) {
+    moveCount = -1;
+    let title;
+    if (isAgainstComputer) {
+      title = isXTurn ? 'You' : 'Computer';
+    } else {
+      title = isXTurn ? 'X' : 'O';
     }
-    isXTurn = !isXTurn;
-    moveCount++;
+    setTimeout(() => {
+      swal({
+        title,
+        text: 'won this round!',
+        button: 'Play again!',
+      }).then(() => {
+        resetGame();
+      });
+    }, 100);
   }
+
+  isXTurn = !isXTurn;
+  moveCount++;
+
   if (moveCount === 9) {
-    swal({
-      title: "It's a DRAW!",
-      button: "Play again!",
-    });
-    resetGame();
+    moveCount = -1;
+    setTimeout(() => {
+      swal({
+        title: "It's a DRAW!",
+        button: 'Play again!',
+      }).then(() => {
+        resetGame();
+      });
+    }, 100);
   }
+};
+
+const computerMove = () => {
+  let hasComputerMoved = false;
+  while (!hasComputerMoved) {
+    const randomId = getRandomSquareId();
+    const square = document.querySelector(`#${randomId}`);
+    if (!square.textContent) {
+      square.textContent = isXTurn ? 'X' : 'O';
+      hasComputerMoved = true;
+    }
+  }
+};
+
+const getRandomSquareId = () => {
+  const randomRow = Math.floor(Math.random() * 3 + 1);
+  const randomColumn = Math.floor(Math.random() * 3 + 1);
+  return `square-${randomRow}${randomColumn}`;
 };
 
 const resetGame = () => {
@@ -47,7 +105,6 @@ const checkForWin = () => {
 };
 
 const checkHorizontal = (row) => {
-  console.log("🚀 ~ checkHorizontal ~ row:", row)
   const compareValue = document.querySelector(`#square-${row}1`).textContent;
   if (!compareValue) return false;
   if (
@@ -59,7 +116,6 @@ const checkHorizontal = (row) => {
 };
 
 const checkVertical = (col) => {
-  console.log("🚀 ~ checkVertical ~ col:", col)
   const compareValue = document.querySelector(`#square-1${col}`).textContent;
   if (!compareValue) return false;
   if (
@@ -71,8 +127,9 @@ const checkVertical = (col) => {
 };
 
 const checkDiagonal = (direction) => {
-  console.log("🚀 ~ checkDiagonal ~ direction:", `#square-${direction ? '3' : '1'}1`)
-  const compareValue = document.querySelector(`#square-${direction ? '3' : '1'}1`).textContent;
+  const compareValue = document.querySelector(
+    `#square-${direction ? '3' : '1'}1`
+  ).textContent;
   const secondValue = document.querySelector(`#square-22`).textContent;
   if (!compareValue || !secondValue || compareValue !== secondValue) {
     return false;
